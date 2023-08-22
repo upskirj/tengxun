47cb7ae064c875629d0d1ec8f3c87d3642df5239
upskirj <106969688+upskirj@users.noreply.github.com>
2023-08-22 18:38:52 +0800

    Add files via upload

---
+++ 10086.js
@@ -0,0 +1,17 @@
+/**
+[rewrite_local]
+^https?://api\.wo\.com\.cn/client/ url script-response-body remove_cmcc_ads.js
+
+[mitm]
+hostname = api.wo.com.cn
+ */
+
+let body = $response.body;
+
+// 判断请求的URL是否包含中国移动开屏广告的关键字
+if (body && /\"ad\":\"splash\"/.test(body)) {
+    // 替换掉广告内容为空
+    body = body.replace(/\"ad\":\"splash\"[^}]+}/g, "");
+}
+
+$done({ body });
