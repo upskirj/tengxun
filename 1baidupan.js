b2b11fa8661cbe36d476a308a8809aff7a46366f
upskirj <106969688+upskirj@users.noreply.github.com>
2023-08-23 10:01:34 +0800

    Add files via upload

---
+++ 1baidupan.js
@@ -0,0 +1,29 @@
+
+// ==UserScript==
+// @name         百度网盘SVIP模拟（简化版）
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  简化版模拟百度网盘SVIP效果
+// @author       Your Name
+// @match        https://pan.baidu.com/*
+// @grant        none
+// ==/UserScript==
+/**
+[rewrite_local]
+^https?:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user url script-response-body baidu_pan_vip.js
+*/
+(function() {
+    'use strict';
+
+    const originalFunc = window.require;
+
+    window.require = function(module) {
+        if (module.indexOf('function-widget-1.4.1.js') > -1) {
+            window.FileUtils = {
+                superVip: 1
+            };
+        }
+        return originalFunc.apply(this, arguments);
+    };
+})();
+
