e24c3f6964633c485b7cb3d7257ad962ffa12182
upskirj <106969688+upskirj@users.noreply.github.com>
2023-09-11 14:11:19 +0800

    Add files via upload

--- filefall.js
+++ filefall.js
@@ -1,3 +1,22 @@
-let body = JSON.parse($response.body);
-body["is_premium"] = true;
-$done({ body: JSON.stringify(body) });
+let body = $response.body;
+let obj = JSON.parse(body);
+
+// 修改用户类型
+obj.user.userType = "premium";
+
+// 修改用户计划信息
+obj.user.plan = {
+  "name": "Premium",
+  "status": "active",
+  "expires": "2099-12-31"
+};
+
+// 修改订阅信息
+obj.subscription = {
+  "status": "active",
+  "expires": "2099-12-31",
+  "cancelAtPeriodEnd": false
+};
+
+body = JSON.stringify(obj);
+$done({ body });
